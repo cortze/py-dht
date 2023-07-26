e3b8e84e3f00860d73c050102376387db3e9e0c1
cortze <cortze@protonmail.com>
2023-07-26 22:08:49 +0200

    add basic benchmarks and results for the dht module

---
+++ benchmarks/benchmark.py
@@ -0,0 +1,93 @@
+import time
+import pandas as pd
+
+
+class NonSucceedBenchmark(Exception):
+    """ Benchmark errors """
+    def __init__(self):
+        pass
+
+
+class Benchmark:
+    """ Benchmark itself :) """
+    def __init__(self, name: str, tag: str, task_to_measure, number_of_times: int):
+        """ receives any kind of processed that needs to be measured, task must return the duration of the process """
+        self.tag = tag
+        self.name = name
+        self.number_of_times = number_of_times
+        self.task_to_measure = task_to_measure
+        self.results = []
+
+    def run(self, timeout: int):
+        # print(f"running benchmark {self.name} ({self.tag})")
+        b_start_time = time.time()
+        for i in range(self.number_of_times):
+            round_result = Result(i)
+            duration = 0.0
+            try: 
+                duration = self.task_to_measure()
+
+            except Exception as e:
+                duration = time.time() 
+                round_result.failed()
+                # print(f"error running benchmark {self.name} ({self.tag}) - {e}")
+
+            round_result.finished(duration)
+            # print(f"round {round_result.round} finished in {round_result.duration_s}s")
+            self.results.append(round_result)
+
+        # print(f"finished benchmark {self.name} ({self.tag}) in {(time.time() - b_start_time)} secs")
+        return self.return_df()
+
+    def return_df(self) -> pd.DataFrame:
+        pd_rows = {
+            'round': [],
+            'start_time': [],
+            'finish_time': [],
+            'task_prep_time': [],
+            'duration_s': [],
+            'failed': []}
+        for r in self.results:
+            pd_rows['round'].append(r.round)
+            pd_rows['start_time'].append(r.start_time)
+            pd_rows['finish_time'].append(r.finish_time)
+            pd_rows['task_prep_time'].append(r.prep_time_s)
+            pd_rows['duration_s'].append(r.duration_s)
+            pd_rows['failed'].append(r.has_failed)
+        try:
+            df = pd.DataFrame(pd_rows)
+        except TypeError as e:
+            print(f"error with benchmark {self.name} ({self.tag})")
+            raise NonSucceedBenchmark()
+        return df
+
+
+class Result:
+    """ Benchmark Results :) """
+    def __init__(self, r: int):
+        self.round = r
+        self.start_time = time.time() 
+        self.finish_time = 0.0 
+        self.duration_s = 0.0 
+        self.prep_time_s = 0.0 
+        self.has_failed = False
+
+    def finished(self, duration: float):
+        self.finish_time = time.time()
+        self.duration_s = duration 
+        self.prep_time_s = (self.finish_time - self.start_time) - self.duration_s
+
+    def failed(self):
+        self.has_failed = True
+
+
+def display_benchmark_metrics(name, df):
+    """ display the summary of a given benchmark-results """
+    print(f'-- benchmark: {name} --')
+    print(f"rounds          : {len(df)}")
+#    print(f'failed rounds   : {df.failed.count()}')
+    print(f'prep time (s)   : {df.task_prep_time.mean()}')
+    print(f"avg (s)         : {df.duration_s.mean()}")
+    print(f"median (s)      : {df.duration_s.median()}")
+    print(f"p90_duration (s): {df.duration_s.quantile(0.9)}")
+
---
+++ benchmarks/network.py
@@ -0,0 +1,134 @@
+import argparse
+import time
+import os
+import random
+from dht import DHTNetwork, DHTClient
+from benchmark import Benchmark, NonSucceedBenchmark, display_benchmark_metrics
+
+def main(args):
+    # check the args
+    tag_base = args.t + "_routing"
+    out_folder = args.o
+    iterations = int(args.i)
+    k = int(args.k)
+    network_size = int(args.n)
+
+    # check if the output folder exists
+    try:
+        os.mkdir(out_folder)
+    except FileExistsError:
+        pass
+    except Exception as e:
+        print(f"benchmark interrupted: {e}")
+        exit(1)
+
+    # -- list of benchmarks related to dht.dht --
+    # 1- network initialization
+    name, result_df = dht_network_initialization(tag_base, iterations, k, network_size)
+    display_benchmark_metrics(name, result_df)
+    result_df.to_csv(out_folder+'/'+name)
+
+    # 2-
+    name, result_df = dht_network_node_bootstrap(tag_base, iterations, k, network_size)
+    display_benchmark_metrics(name, result_df)
+    result_df.to_csv(out_folder+'/'+name)
+
+    # 3-
+    name, result_df = dht_network_bootstrap(tag_base, iterations, k, network_size)
+    display_benchmark_metrics(name, result_df)
+    result_df.to_csv(out_folder+'/'+name)
+
+    exit(0)
+
+
+def gen_network(k: int, network_size: int):
+    node_ids = random.sample(range(1, 4*network_size), network_size)
+    network = DHTNetwork(networkID=0, errorRate=0)
+    for node in node_ids:
+        network.add_new_node(node)
+    return network, node_ids
+
+
+def get_dht_cli(network):
+    random_id = random.sample(range(network.len()), 1)[0]
+    dht_cli = network.network.nodeStore.get_node(random_id)
+    return dht_cli
+
+
+def dht_network_initialization(tag_base: str, i: int, k: int, network_size: int):
+    """ benchmarks the time it takes to find the closest nodes to a key in the rt """
+    b_name = tag_base + f'_network_initialization'
+
+    def task() -> float:
+        # measurement
+        start = time.time()
+        _, _ = gen_network(k, network_size)
+        return time.time() - start
+
+    b = Benchmark(
+        name='network_initialization',
+        tag=tag_base,
+        task_to_measure=task,
+        number_of_times=i)
+    df = b.run(timeout=0)
+    return b_name, df
+
+
+def dht_network_node_bootstrap(tag_base: str, i: int, k: int, network_size: int):
+    """ benchmarks the time it takes to find the closest nodes to a key in the rt """
+    b_name = tag_base + f'_network_node_bootstrap'
+
+    def task() -> float:
+        # init
+        n, _ = gen_network(k, network_size)
+        p = get_dht_cli(n)
+
+        # measurement
+        start = time.time()
+        _ = n.bootstrap_node(p.ID, k, 100)
+        return time.time() - start
+
+    b = Benchmark(
+        name='node_bootstrap',
+        tag=tag_base,
+        task_to_measure=task,
+        number_of_times=i)
+    df = b.run(timeout=0)
+    return b_name, df
+
+def dht_network_bootstrap(tag_base: str, i: int, k: int, network_size: int):
+    """ benchmarks the time it takes to find the closest nodes to a key in the rt """
+    b_name = tag_base + f'_network_node_bootstrap'
+
+    def task() -> float:
+        # init
+        n, ids = gen_network(k, network_size)
+
+        # measurement
+        start = time.time()
+        for id in ids:
+            _ = n.bootstrap_node(id, k, 100)
+        return time.time() - start
+
+    b = Benchmark(
+        name='node_bootstrap',
+        tag=tag_base,
+        task_to_measure=task,
+        number_of_times=i)
+    df = b.run(timeout=0)
+    return b_name, df
+
+
+
+if __name__ == "__main__":
+    """ run the benchmarks under the given tag and parameters """
+    args = argparse.ArgumentParser()
+    args.add_argument('-t')  # test tag
+    args.add_argument('-o')  # output folder
+    args.add_argument('-i')  # number of iterations (for statistical robustness)
+    args.add_argument('-k')  # bucket size
+    args.add_argument('-n')  # network size (to compose the rt)
+    a = args.parse_args()
+    main(a)
+
+
