64e1112e2c3b749dd2207dd6f5d7b98f3c108772
cortze <cortze@protonmail.com>
2023-07-28 17:29:59 +0200

    swap shitty string representation for optimal bitarrays

--- dht/hashes.py
+++ dht/hashes.py
@@ -1,14 +1,16 @@
 import ctypes
+from bitarray.util import int2ba
 
 # TODO: swapt hash to SHA256 with the possibility of reusing a given seed for reproducibility
 # at the moment, I'm using the default 64bit hash function from Python
 HASH_BASE = 64
 
-class Hash():
+
+class Hash:
     def __init__(self, value):
         """ basic representation of a Hash object for the DHT, which includes the main utilities related to a hash """
         self.value = self.hash_key(value)
-        self.bitArray = BitArray(self.value, HASH_BASE)
+        self.bitarray = BitArray(self.value, HASH_BASE)
         # TODO: the hash values could be reproduced if the ENVIRONMENT VARIABLE PYTHONHASHSEED is set to a 64 bit integer https://docs.python.org/3/using/cmdline.html#envvar-PYTHONHASHSEED
 
     def hash_key(self, key):
@@ -20,62 +22,41 @@ class Hash():
         # ensure that the hash is unsigned
         return ctypes.c_ulong(h).value
  
-    def xor_to(self, targetHash:int) -> int:
+    def xor_to(self, targetint: int) -> int:
         """ Returns the XOR distance between both hash values"""
-        distance = self.value ^ targetHash
-        return ctypes.c_ulong(distance).value
+        return ctypes.c_ulong(self.value ^ targetint).value
 
-    def xor_to_hash(self, targetHash) -> int:
+    def xor_to_hash(self, targethash) -> int:
         """ Returns the XOR distance between both hash values"""
-        distance = self.value ^ targetHash.value
-        return ctypes.c_ulong(distance).value
+        return ctypes.c_ulong(self.value ^ targethash.value).value
 
-    def shared_upper_bits(self, targetHash) -> int:
+    def shared_upper_bits(self, targethash) -> int:
         """ returns the number of upper sharing bits between 2 hash values """
-        targetBits = BitArray(targetHash.value, HASH_BASE)
-        sBits = self.bitArray.upper_sharing_bits(targetBits)
-        return sBits
+        return self.bitarray.upper_sharing_bits(targethash.bitarray)
 
     def __repr__(self) -> str:
         return str(hex(self.value))
 
-    def __eq__(self, targetHash) -> bool:
-        return self.value == targetHash.value
+    def __eq__(self, targethash) -> bool:
+        return self.value == targethash.value
 
-    def is_smaller_than(self, targetHash) -> bool:
-        return self.value < targetHash.value 
 
-    def is_greater_than(self, targetHash) -> bool:
-        return self.value > targetHash.value 
-
-class BitArray():
+class BitArray:
     """ array representation of an integer using only bits, ideal for finding matching upper bits"""
-    def __init__(self, intValue:int, base:int):
+    def __init__(self, uintval:int, base:int):
         self.base = base
-        self.bitArray = self.bin(intValue)
+        self.bitarray = int2ba(uintval, length=base)
 
     def __repr__(self):
-        return str(self.bitArray)
+        return self.bitarray.to01()
 
-    def upper_sharing_bits(self, targetBitArray) -> int:
-        sBits = 0
-        for i, bit in enumerate(self.bitArray):
-            if bit == targetBitArray.get_x_bit(i):
-                sBits += 1
+    def upper_sharing_bits(self, targetba) -> int:
+        sbits = 0
+        proc = self.bitarray ^ targetba.bitarray
+        for bit in proc:
+            if bit == 0:
+                sbits += 1
             else:
                 break 
-        return sBits
+        return sbits
 
-    def get_x_bit(self, idx:int = 0):
-        return self.bitArray[idx]
-
-    def bin(self, n):
-        s = ""
-        i = 1 << self.base-1
-        while(i > 0) :
-            if((n & i) != 0) :
-                s += "1"
-            else :
-                s += "0"
-            i = i // 2
-        return s
